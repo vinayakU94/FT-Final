a511907b8b414417d40a9832b96df2ac539ba7a5
gauravghodinde <gauravghodinde@gmail.com>
2024-07-18 16:03:09 +0530

    image upload to cloudinart

--- src/routes/repair_request.routes.js
+++ src/routes/repair_request.routes.js
@@ -1,10 +1,25 @@
 import { Router } from "express";
-import { addRepairRequest, getRepairRequest, getAllRepairRequests } from "../controllers/repair_request.controller.js";
+import {
+  addRepairRequest,
+  getRepairRequest,
+  getAllRepairRequests,
+} from "../controllers/repair_request.controller.js";
+
+
+import { upload } from "../middleware/multer.middleware.js";
 
 
 const router = Router();
 
-router.route("/add").post(addRepairRequest);
+router.route("/add").post(
+  upload.fields([
+    {
+      name: "image",
+      maxCount: 1,
+    },
+  ]),
+  addRepairRequest
+);
 
 router.route("/get").get(getRepairRequest);
 router.route("/getAll").get(getAllRepairRequests);
