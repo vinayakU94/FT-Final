ff6464c30b997146b7d122d322ea981581ca2957
gauravghodinde <gauravghodinde@gmail.com>
2024-07-21 02:43:27 +0530

    ok

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,14 +8,3 @@ router.route("/signup").post(registerUser);
 router.route("/login").post(loginUser);
 
 export default router;
-
-import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
-
-const router = Router();
-
-router.route("/signup").post(registerUser);
-
-router.route("/login").post(loginUser);
-
-export default router;
