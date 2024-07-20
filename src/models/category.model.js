58129edd8f618d4743c61ae07ae8a43457795377
gauravghodinde <gauravghodinde@gmail.com>
2024-07-21 02:40:32 +0530

    idk

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -14,20 +14,3 @@ const categorySchema = new Schema(
 );
 
 export const Category =  model("Category", categorySchema);
-
-
-import { Schema, model } from "mongoose";
-
-const categorySchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
-
-export const Category =  model("Category", categorySchema);
